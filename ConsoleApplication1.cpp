4bb86666273b2c55dca9da8b60e086453494e973
erikoui <eriksofs@hotmail.com>
2020-08-22 14:28:39 +0300

    done

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -2,17 +2,19 @@
 //
 #include <vector>
 #include <iostream>
-int decksize = 52;
+#include <ctime>
+#include <cstdlib>
+int decksize = 52*4;
 int handsize = 4;
 
-void printhand(vector<int>& hand) {
+void printhand(std::vector<int>& hand) {
     int i;
     for (i = 0; i < hand.size(); i++) {
-        cout << hand[i] << " ";
+        std::cout << hand[i] << " ";
     }
-    cout << endl;
+    std::cout << std::endl;
 }
-void filldeck(vector<int>& deck) {
+void filldeck(std::vector<int>& deck) {
     deck.clear();
     deck.resize(decksize);
     int i;
@@ -21,8 +23,9 @@ void filldeck(vector<int>& deck) {
     }
 }
 
-void shuffledeck(vector<int>& deck,vector<int>& scarts) {
+void shuffledeck(std::vector<int>& deck, std::vector<int>& scarts) {
     int i, i1, i2, temp;
+    scarts.clear();
     for (i = 0; i < 3 * deck.size(); i++) {
         i1 = rand() % deck.size();
         i2 = rand() % deck.size();
@@ -37,7 +40,8 @@ void shuffledeck(vector<int>& deck,vector<int>& scarts) {
     deck.erase(deck.begin());
 }
 
-void deal(vector<int>& deck, vector<int>& hand) {
+void deal(std::vector<int>& deck, std::vector<int>& hand) {
+    hand.clear();
     int i;
     for (i = 0; i < handsize; i++) {
         hand.push_back(deck[0]);
@@ -45,30 +49,97 @@ void deal(vector<int>& deck, vector<int>& hand) {
     }
 }
 
-class player{
+class player {
     int playstyle;
     int points;
     player() {
         playstyle = 0;
-
+        points = 0;
     }
 
     void play(std::vector<int>& hand, std::vector<int>& deck) {
         if (playstyle == 0) {//random moves
-            cout << "aa";
+            std::cout << "aa";
         }
     }
-}
+};
 
 
 int main()
 {
+    srand(time(NULL));
     std::cout << "Hello World!\n";
-    std::vector<int> deck, hand, scarts;
-    filldeck(deck);
-    shuffledeck(deck,scarts);
-    deal(hand, deck);
-    printhand(hand);
+    std::vector<int> deck, RANDhand, MAXhand, MIDhand, scarts;
+    int MAXsum, RANDsum;
+    int i;
+    int max_turns = 1;
+    for(max_turns=1;max_turns<100;max_turns++){
+        MAXsum = 0;
+        RANDsum = 0;
+        for (i = 0; i < 1000; i++) {
+            //shuffle and deal
+            filldeck(deck);
+            shuffledeck(deck, scarts);
+            deal(deck, RANDhand);
+            deal(deck, MAXhand);
+            deal(deck, MIDhand);
+
+            int j;
+            //play the game
+            for (j = 0; j < max_turns; j++) {
+                //rando plays
+                int r = rand() % 2;
+                int s = rand() % 4;
+                if (r == 1) {//draw from deck
+                    scarts[0]=RANDhand[s];
+                    RANDhand[s] = deck[0];
+                    deck.erase(deck.begin());
+                }
+                else {
+                    int t = scarts[0];
+                    scarts[0] = RANDhand[s];
+                    RANDhand[s] = t;
+                }
+
+                //max diff plays
+                int c;
+                if (scarts[0] < 7) {//pick it up
+                    c = scarts[0];
+                }
+                else {
+                    c = deck[0];
+                    deck.erase(deck.begin());
+                }
+
+                if ((MAXhand[0] - c) > (6.5 - c)) {//put in left better option than mid
+                    if ((MAXhand[3] - c) > (MAXhand[0] - c)){//put in right better option than left
+                        scarts[0] = MAXhand[3];
+                        MAXhand[3] = c;
+                    }
+                    else {
+                        scarts[0] = MAXhand[0];
+                        MAXhand[0] = c;
+                    }
+                }
+                else {//put it in some random place cuz i cba to keep track of it
+                    if (c < 7) {
+                        s = rand() % 2 + 1;
+                        scarts[0] = MAXhand[s];
+                        MAXhand[0] = c;
+                    }
+                    else {
+                        scarts[0] = c;
+                    }
+                }
+            }
+            //add up stats
+            for (j = 0; j < 4; j++) {
+                RANDsum += RANDhand[j] > 10 ? 10 : RANDhand[j];
+                MAXsum += MAXhand[j] > 10 ? 10 : MAXhand[j];
+            }
+        }
+        std::cout << "For " << max_turns << " turns, max diff has " << MAXsum / 100 << " points and rando has " << RANDsum / 100 << " points on average" << std::endl;
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
